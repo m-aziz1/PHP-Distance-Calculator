cd2879e4a0c0fa035de1f3d5b1b28a1196215b82
MUHAMMAD AZIZ <m.aziz5@share.epsb.ca>
2022-06-14 14:13:33 -0600

    Finish base functionality

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,11 +14,11 @@
     <img src="./images/distance-between-2-points.jpg" alt="Triangle Diagram">
     <form action="./index.php" method="$_GET">
         <p>Point 1</p>
-        <input type="number" name="x1" placeholder="x1">
-        <input type="number" name="y1" placeholder="y1">
+        <input type="number" name="x1" placeholder="x1" required>
+        <input type="number" name="y1" placeholder="y1" required>
         <p>Point 2</p>
-        <input type="number" name="x2" placeholder="x2">
-        <input type="number" name="y2" placeholder="y2">
+        <input type="number" name="x2" placeholder="x2" required>
+        <input type="number" name="y2" placeholder="y2" required>
         <br>
         <button type="submit">Submit</button>
     </form>
@@ -27,16 +28,16 @@
 </html>
 
 <?php
-$keys = ["x1", "y1", "x2", "y2"];
-$arrLength = count($_GET);
+$responseLength = count($_GET);
 
-for ($i = 0; $i < $arrLength; $i++) {
-    if (isset($_GET[$keys[$i]])) {
-        echo " is set";
-    }
+// If Form Submitted
+if ($responseLength > 0) {
+    // Calculate and Output
+    extract($_GET);
+    $x = $x2 - $x1;
+    $y = $y2 -$y1;
+    $d = ($x**2 + $y**2)**0.5;
+
+    echo "Distance between points is {$d}";
 }
-
-// extract($_GET);
-// echo $x1;
-
 ?>
